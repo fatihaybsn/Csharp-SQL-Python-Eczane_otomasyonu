b2099aaa7a83f62dcb12b4c5b9c2b23bb6e49266
Fatih AYIBASAN <fathaybasn@gmail.com>
2025-02-02 23:42:22 +0300

    Create langues.sh

---
+++ langues.sh
@@ -0,0 +1,7 @@
+touch main.py   # Python dosyası
+touch main.cs   # C# dosyası
+touch database.sql   # MSSQL dosyası
+
+git add main.py main.cs database.sql
+git commit -m "Add placeholder files for language detection"
+git push origin main  # Veya master
